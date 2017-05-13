adc532518e603f98217d86ddf20200d5a43d1be4
linyoumu <848516118@qq.com>
2017-05-13 23:33:49 +0800

    将头部和底部分离出来做成公用的部分

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by linapple on 2017/5/13.
+ */
+document.writeln("<footer id=\'footer\'>");
+document.writeln("            <div id=\'footer-info\' class=\'outer\'>");
+document.writeln("                &copy; 2015 Lin You Mu");
+document.writeln("            </div>");
+document.writeln("        </footer>");
---
+++ js/recentArticle.js
@@ -0,0 +1,27 @@
+/**
+ * Created by linapple on 2017/5/13.
+ */
+document.writeln("<aside id=\'sidebar\'>");
+document.writeln("            <div class=\'widget-wrap\'>");
+document.writeln("                <h3 class=\'widget-title\'>档案</h3>");
+document.writeln("                <div class=\'widget\'>");
+document.writeln("                    <ul class=\'archive-list\'><li class=\'archive-list-item\'><a class=\'archive-list-link\' href=\'/\'>所有文章</a></li></ul>");
+document.writeln("                </div>");
+document.writeln("            </div>");
+document.writeln("            <div class=\'widget-wrap\'>");
+document.writeln("                <h3 class=\'widget-title\'>最近的文章</h3>");
+document.writeln("                <div class=\'widget\'>");
+document.writeln("                    <ul>");
+document.writeln("                        <li>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/Animations.html\'>iOS开发中三种简单的动画设置</a>");
+document.writeln("                        </li>");
+document.writeln("                        <li>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/NSArray.html\'>NSArray的基本使用</a>");
+document.writeln("                        </li>");
+document.writeln("                        <li>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/NSString.html\'>NSString的常用用法</a>");
+document.writeln("                        </li>");
+document.writeln("                    </ul>");
+document.writeln("                </div>");
+document.writeln("            </div>");
+document.writeln("        </aside>");
