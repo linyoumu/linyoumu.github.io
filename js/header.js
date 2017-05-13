468a67ce4ae7eb82293feca697d91e69de002252
linyoumu <848516118@qq.com>
2017-05-13 23:19:43 +0800

    将头部和底部分离出来做成公用的部分

---
+++ js/header.js
@@ -0,0 +1,13 @@
+/**
+ * Created by linapple on 2017/5/13.
+ */
+document.writeln("<header id=\'header\'>");
+document.writeln("            <div id=\'banner\'></div>");
+document.writeln("            <div id=\'header-outer\' class=\'outer\'>");
+document.writeln("                <div id=\'header-title\' class=\'inner\'>");
+document.writeln("                    <h1 id=\'logo-wrap\'>");
+document.writeln("                        <a href=\'/\' id=\'logo\'>欢迎来到我的个人博客</a>");
+document.writeln("                    </h1>");
+document.writeln("                </div>");
+document.writeln("            </div>");
+document.writeln("        </header>");
