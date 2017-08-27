b1b2ff0a2cb62a5d489e9f71a204e2d964c67482
linyoumu <848516118@qq.com>
2017-08-27 17:42:42 +0800

    编辑文章

--- js/recentArticle.js
+++ js/recentArticle.js
@@ -13,13 +13,13 @@ document.writeln("                <h3 class=\'widget-title\'>最近的文章</h3
 document.writeln("                <div class=\'widget\'>");
 document.writeln("                    <ul>");
 document.writeln("                        <li>");
-document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/HttpForIOS.html\'>Http for iOS</a>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/ProcedurePack.html\'>打包过程</a>");
 document.writeln("                        </li>");
 document.writeln("                        <li>");
-document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/UIWebViewAndWKWebView.html.html\'>UIWebView与WKWebView的基本使用</a>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/AboutCertificate.html.html\'>证书相关</a>");
 document.writeln("                        </li>");
 document.writeln("                        <li>");
-document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/Animations.html\'>iOS开发中三种简单的动画设置</a>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/WifiSmarkDevice.html\'>智能交互 - app配置智能硬件解决方案</a>");
 document.writeln("                        </li>");
 document.writeln("                    </ul>");
 document.writeln("                </div>");
