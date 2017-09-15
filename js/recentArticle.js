7c5b2dd9d330ce9efe10390d9ad9a8728100b547
linyoumu <848516118@qq.com>
2017-09-15 10:49:53 +0800

    新增文章-iOS之BLE DFU升级

--- js/recentArticle.js
+++ js/recentArticle.js
@@ -13,13 +13,13 @@ document.writeln("                <h3 class=\'widget-title\'>最近的文章</h3
 document.writeln("                <div class=\'widget\'>");
 document.writeln("                    <ul>");
 document.writeln("                        <li>");
+document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/BLEWithDFU.html\'>iOS之BLE DFU升级</a>");
+document.writeln("                        </li>");
+document.writeln("                        <li>");
 document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/ProcedurePack.html\'>打包过程</a>");
 document.writeln("                        </li>");
 document.writeln("                        <li>");
 document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/AboutCertificate.html.html\'>证书相关</a>");
-document.writeln("                        </li>");
-document.writeln("                        <li>");
-document.writeln("                            <a href=\'/MyBlog/Articles/iOS/Basic/WifiSmarkDevice.html\'>智能交互 - app配置智能硬件解决方案</a>");
 document.writeln("                        </li>");
 document.writeln("                    </ul>");
 document.writeln("                </div>");
